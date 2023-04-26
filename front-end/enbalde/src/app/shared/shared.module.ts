603c7dc0c3909609886f96ff8d79472ed8b622b1
Roberto <rpgrca@gmail.com>
2023-04-25 21:13:05 -0300

    agregar barra de navegacion inicial

--- front-end/enbalde/src/app/shared/shared.module.ts
+++ front-end/enbalde/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,11 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    HeaderComponent, FooterComponent
   ]
 })
 export class SharedModule { }
