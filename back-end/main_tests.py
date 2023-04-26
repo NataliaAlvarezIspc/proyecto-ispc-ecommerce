bf9a2315be9b552bc7f6e6af2b6c245165bd3acd
Roberto <rpgrca@gmail.com>
2023-04-25 23:32:16 -0300

    agregar tests para usuario

--- back-end/main_tests.py
+++ back-end/main_tests.py
@@ -2,11 +2,13 @@ import unittest
 from tests.articulo_tests import ArticuloTests
 from tests.venta_tests import VentaTests
 from tests.seleccion_tests import SeleccionTests
+from tests.usuario_tests import UsuarioTests
 
 suite = unittest.TestSuite()
 
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ArticuloTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(VentaTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(SeleccionTests))
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(UsuarioTests))
 
 unittest.TextTestRunner(verbosity=3).run(suite)
