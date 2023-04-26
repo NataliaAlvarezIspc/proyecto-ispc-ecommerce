eaea9346123eb5ea4b45deb48bfecc152abd5372
Roberto <rpgrca@gmail.com>
2023-04-25 23:38:37 -0300

    agregar test para cliente

--- back-end/main_tests.py
+++ back-end/main_tests.py
@@ -3,6 +3,7 @@ from tests.articulo_tests import ArticuloTests
 from tests.venta_tests import VentaTests
 from tests.seleccion_tests import SeleccionTests
 from tests.usuario_tests import UsuarioTests
+from tests.cliente_tests import ClienteTests
 
 suite = unittest.TestSuite()
 
@@ -10,5 +11,6 @@ suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ArticuloTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(VentaTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(SeleccionTests))
 suite.addTest(unittest.TestLoader().loadTestsFromTestCase(UsuarioTests))
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ClienteTests))
 
 unittest.TextTestRunner(verbosity=3).run(suite)
