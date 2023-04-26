e7e82ea396d86e4980c4ad92e28acabbda6ad5c1
Roberto <rpgrca@gmail.com>
2023-04-25 22:47:04 -0300

    agregar tipos de envio

--- back-end/model/envio.py
+++ back-end/model/envio.py
@@ -1,2 +1,38 @@
-class Envio:
-    pass
+from abc import ABC, abstractmethod
+
+class Envio(ABC):
+    @abstractmethod
+    def obtener_costo(self):
+        pass
+
+    @abstractmethod
+    def obtener_descripcion(self):
+        pass
+
+class RetiroEnTienda(Envio):
+    def obtener_costo(self):
+        return 0
+
+    def obtener_descripcion(self):
+        return "Retiro en tienda"
+
+class EnvioLocal(Envio):
+    def obtener_costo(self):
+        return 150
+
+    def obtener_descripcion(self):
+        return "Envío dentro de la ciudad"
+
+class EnvioCiudadesCercanas(Envio):
+    def obtener_costo(self):
+        return 350
+
+    def obtener_descripcion(self):
+        return "Envío a ciudades cercanas"
+
+class EnvioCiudadesAlejadas(Envio):
+    def obtener_costo(self):
+        return 1400
+
+    def obtener_descripcion(self):
+        return "Envío a ciudades alejadas"
