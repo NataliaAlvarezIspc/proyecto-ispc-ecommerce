8417a040a24d39069654f96af911ac313383d4fc
Brian Almada <brianalexisalmada84@gmail.com>
2023-04-27 22:34:26 -0300

    Haciendo el routing de componente catalogo

--- front-end/enbalde/src/app/app-routing.module.ts
+++ front-end/enbalde/src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { HomeComponent } from './pages/home/home.component';
-
+import { CatalogComponent } from './pages/catalogo/catalogo.component'
 import { RegistracionComponent } from './pages/registracion/registracion.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contacto', component: ContactoComponent },
-  
+  { path: 'catalogo', component: CatalogComponent},
   { path: 'registracion', component: RegistracionComponent}
 ];
 
