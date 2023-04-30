55332c7c62466737d0d04ce224291cf1cacaebf8
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-04-29 23:13:04 -0300

    [ADD] Home - Stylos - Prueba

--- front-end/enbalde/src/app/pages/pages.module.ts
+++ front-end/enbalde/src/app/pages/pages.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactoComponent } from './contacto/contacto.component';
-import { RegistracionComponent } from './registracion/registracion.component';
-import { AltaProductoComponent } from './alta-producto/alta-producto.component';
-import { CatalogComponent } from './catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ContactoComponent,
-    RegistracionComponent,
-    AltaProductoComponent,
-    CatalogComponent
+    ContactoComponent
   ],
   imports: [
     CommonModule,
