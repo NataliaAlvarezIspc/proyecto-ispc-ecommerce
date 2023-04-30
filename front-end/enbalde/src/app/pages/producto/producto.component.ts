cc0faa01740d9162aed775ea29cdde838cc618d4
Brian Almada <brianalexisalmada84@gmail.com>
2023-04-30 03:10:21 -0300

    Clase exportadora para 'title' por medio del selector 'app-producto'

--- front-end/enbalde/src/app/pages/producto/producto.component.ts
+++ front-end/enbalde/src/app/pages/producto/producto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-producto',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
+
+  @Input() title: string = '';
 
 }
