99c0b618edac9242ff3514158ff961cdfc5ed41b
Brian Almada <brianalexisalmada84@gmail.com>
2023-05-01 22:08:54 -0300

    Cambiando el código que tenía error y probando una nueva manera

--- front-end/enbalde/src/app/pages/producto/producto.component.ts
+++ front-end/enbalde/src/app/pages/producto/producto.component.ts
@@ -1,19 +1,17 @@
-  import { Component, Input } from '@angular/core';
-  import { Producto } from '../producto/modelo/modelo.producto';
+import { Component, Input } from '@angular/core';
+import { Producto } from '../producto/modelo/modelo.producto';
 
-
-  @Component({
-    selector: 'app-producto',
-    templateUrl: './producto.component.html',
-    styleUrls: ['./producto.component.css']
-  })
-  export class ProductoComponent {
-
-    @Input() producto: Producto = {
-      titulo: 'Tentatenbalde Chocolate',
-      descripcion:'Helado de chocolate artesanal',
-      precio: 0,
-      imagen: ["/src/assets/img/HeladoChocolate.jpeg"],
-      id: 0
-  }
-  }
+@Component({
+  selector: 'app-producto-catalogo',
+  templateUrl: './producto.component.html',
+  styleUrls: ['./producto.component.css']
+})
+export class ProductoComponent {
+  @Input() producto: Producto = {
+    id: 0,
+    titulo: 'Tentatenbalde Chocolate',
+    descripcion: 'Helado de chocolate artesanal',
+    precio: 0,
+    imagen: ['/src/assets/img/HeladoChocolate.jpeg']
+  };
+}
