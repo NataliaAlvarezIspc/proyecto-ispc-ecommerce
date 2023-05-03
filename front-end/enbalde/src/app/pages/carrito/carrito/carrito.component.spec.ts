1e8f05238d0f810d78c57fbeed5571d7bfa8a3df
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-02 23:27:10 -0300

    [ADD] Creacion de paquete carrito junto con su componente

---
+++ front-end/enbalde/src/app/pages/carrito/carrito/carrito.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { CarritoComponent } from './carrito.component';
+
+describe('CarritoComponent', () => {
+  let component: CarritoComponent;
+  let fixture: ComponentFixture<CarritoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CarritoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarritoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
