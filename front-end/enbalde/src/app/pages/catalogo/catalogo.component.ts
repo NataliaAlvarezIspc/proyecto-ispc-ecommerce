7e14918c30d347dedc9861bd4830bd55cda58265
Brian Almada <brianalexisalmada84@gmail.com>
2023-05-02 22:21:59 -0300

    Agregando dos productos más al arreglo

--- front-end/enbalde/src/app/pages/catalogo/catalogo.component.ts
+++ front-end/enbalde/src/app/pages/catalogo/catalogo.component.ts
@@ -11,6 +11,8 @@ export class CatalogComponent {
     new ProductoClass(1, "Tentatenbalde Chocolate", "Helado sabor chocolate artesanal", 1100, "/assets/no_image_available.svg"),
     new ProductoClass(2, "Tentatenbalde Frutilla", "Helado sabor frutilla artesanal", 1100, "/assets/no_image_available.svg"),
     new ProductoClass(3, "Tentatenbalde Dulce de Leche", "Helado sabor dulce de leche artesanal", 1100, "/assets/no_image_available.svg"),
-    new ProductoClass(4, "Tentatenbalde Choco-frutilla", "Helado sabor chocolate y frutilla artesanal", 1100, "/assets/no_image_available.svg")
+    new ProductoClass(4, "Tentatenbalde Choco-frutilla", "Helado sabor chocolate y frutilla artesanal", 1200, "/assets/no_image_available.svg"),
+    new ProductoClass(4, "Tentatenbalde frutilla-Americana", "Helado sabor frutilla y crema americana artesanal", 1200, "/assets/no_image_available.svg"),
+    new ProductoClass(4, "Tentatenbalde D.Leche-Chocolate", "Helado sabor dulce de leche y chocolate artesanal", 1200, "/assets/no_image_available.svg")
   ];
 }
