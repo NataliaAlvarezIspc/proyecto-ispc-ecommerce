5ff30655d2115fa278ab477f5bfb15e39842c544
Brian Almada <brianalexisalmada84@gmail.com>
2023-05-03 20:10:56 -0300

    Agregando imagenes momentaneas de pote de helado en las card de catalogo

--- front-end/enbalde/src/app/pages/catalogo/catalogo.component.ts
+++ front-end/enbalde/src/app/pages/catalogo/catalogo.component.ts
@@ -8,11 +8,11 @@ import { Producto, ProductoClass } from '../producto/modelo/modelo.producto';
 })
 export class CatalogComponent {
   @Input() productos: Producto [] = [
-    new ProductoClass(1, "Tentatenbalde Chocolate", "Helado sabor chocolate artesanal", 1100, "/src/assets/img/chocolate.jpg"),
-    new ProductoClass(2, "Tentatenbalde Frutilla", "Helado sabor frutilla artesanal", 1100, "/src/assets/img/chocolate.jpg"),
-    new ProductoClass(3, "Tentatenbalde Dulce de Leche", "Helado sabor dulce de leche artesanal", 1100, "/src/assets/img/chocolate.jpg"),
-    new ProductoClass(4, "Tentatenbalde Choco-frutilla", "Helado sabor chocolate y frutilla artesanal", 1200, "/src/assets/img/choco-frutilla.jpg"),
-    new ProductoClass(4, "Tentatenbalde frutilla-Americana", "Helado sabor frutilla y crema americana artesanal", 1200, "/src/assets/img/choco-frutilla.jpg"),
-    new ProductoClass(4, "Tentatenbalde D.Leche-Chocolate", "Helado sabor dulce de leche y chocolate artesanal", 1200, "/src/assets/img/choco-frutilla.jpg")
+    new ProductoClass(1, "Tentatenbalde Chocolate", "Helado sabor chocolate artesanal", 1100, "/assets/no_image_available.svg"),
+    new ProductoClass(2, "Tentatenbalde Frutilla", "Helado sabor frutilla artesanal", 1100, "/assets/no_image_available.svg"),
+    new ProductoClass(3, "Tentatenbalde Dulce de Leche", "Helado sabor dulce de leche artesanal", 1100, "/assets/no_image_available.svg"),
+    new ProductoClass(4, "Tentatenbalde Choco-frutilla", "Helado sabor chocolate y frutilla artesanal", 1200, "/assets/no_image_available.svg"),
+    new ProductoClass(4, "Tentatenbalde frutilla-Americana", "Helado sabor frutilla y crema americana artesanal", 1200, "/assets/no_image_available.svg"),
+    new ProductoClass(4, "Tentatenbalde D.Leche-Chocolate", "Helado sabor dulce de leche y chocolate artesanal", 1200, "/assets/no_image_available.svg")
   ];
 }
