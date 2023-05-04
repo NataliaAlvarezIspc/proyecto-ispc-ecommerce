673268c71aa3a0288da79c806aaa84072b5330af
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-04 14:00:44 -0300

    [UPDATE] Boton carrito card - funcion Agregar - img card terminado

--- front-end/enbalde/src/app/pages/carrito/carrito/carrito.component.ts
+++ front-end/enbalde/src/app/pages/carrito/carrito/carrito.component.ts
@@ -9,21 +9,39 @@ import { Producto, ProductoClass } from '../../producto/modelo/modelo.producto';
 export class CarritoComponent  {
 
   total: number = 0
-  carrito: Producto[] = [];
+  //carrito: Producto[] = [];
+  totalCarrito: number;
+
+  constructor() {
+    this.totalCarrito = this.carritoSuma();
+  }
 
   @Input() productos: Producto [] = [
-    new ProductoClass(1, "Helado Tentación Dark", "Sabor a crema chocolatozo", 1100, 3, "/assets/no_image_available.svg"),
-    new ProductoClass(2, "Helado Tentación White", "Sabor a fresco granizado", 500, 3,  "/assets/no_image_available.svg"),
-    new ProductoClass(3, "Helado Tentación Danger", "Sabor a crema frutilla", 300, 3,  "/assets/no_image_available.svg"),
-    new ProductoClass(4, "Helado Tentación Angel", "Sabor a crema del cielo", 300, 3,  "/assets/no_image_available.svg")
+    new ProductoClass(1, "Helado Tentación Dark", "Sabor a crema chocolatozo", 1100, 3, "/assets/img/chocolate.jpg"),
+    new ProductoClass(2, "Helado Tentación White", "Sabor a fresco granizado", 500, 3,  "/assets/img/chocolate.jpg"),
+    // new ProductoClass(3, "Helado Tentación Danger", "Sabor a crema frutilla", 300, 3,  "/assets/img/chocolate.jpg"),
+    // new ProductoClass(4, "Helado Tentación Angel", "Sabor a crema del cielo", 300, 3,  "/assets/img/chocolate.jpg")
 
   ];
+  
+  @Input() carrito: Producto [] = [
+    new ProductoClass(1, "Helado Tentación Dark", "Sabor a crema chocolatozo", 1100, 3,"/assets/img/chocolate.jpg"),
+    new ProductoClass(2, "Helado Tentación White", "Sabor a fresco granizado", 500, 3,"/assets/img/chocolate.jpg")
+  ];
 
+  carritoSuma(): number {
+    let total = 0;
+    for(let i = 0; i < this.carrito.length; i++) {
+      total += this.carrito[i].precio;
+    }
+    return total;
+  }
 
 // Elimino todos los productos una vez pagados y restauro el valor total
   pagar(){
     alert('Has pagado correctamente');
     this.total = 0;
+    this.totalCarrito = 0; // Cree esta variable solamente para poder hacer uso del totalCarrito
     this.carrito = [];
     const carritoReducido = this.getCarritoReducido();
     alert('¡Su producto ya está en camino!')
@@ -40,6 +58,8 @@ export class CarritoComponent  {
       alert('No hay mas helado disponible de: '+ producto.titulo)
     }
   }
+
+
 // Elimino un producto al carrito
   eliminarDelCarrito(producto: Producto) {
     const index = this.carrito.findIndex(p => p.id === producto.id);
@@ -49,6 +69,8 @@ export class CarritoComponent  {
       producto.cantidadDisponible++;
     }
   }
+
+
  // Creo un array para almacenar los elementos repetidos
   getCarritoReducido(){
     const carritoReducido: any[] = [];
