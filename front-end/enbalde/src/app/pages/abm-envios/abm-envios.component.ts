cd322628b1802d5ca9e42587e0f19d5e8f5f8c01
Roberto <rpgrca@gmail.com>
2023-05-06 15:13:01 -0300

    agregar componente para abm envios

---
+++ front-end/enbalde/src/app/pages/abm-envios/abm-envios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abm-envios',
+  templateUrl: './abm-envios.component.html',
+  styleUrls: ['./abm-envios.component.css']
+})
+export class AbmEnviosComponent {
+
+}
