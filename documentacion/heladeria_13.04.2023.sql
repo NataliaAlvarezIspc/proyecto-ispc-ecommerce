a67042347a9d5ff328e5b044bf53a091df634adb
Roberto <rpgrca@gmail.com>
2023-05-07 16:35:57 -0300

    agregar unique auto_increment, quitar indices

--- documentacion/heladeria_13.04.2023.sql
+++ documentacion/heladeria_13.04.2023.sql
@@ -18,29 +18,29 @@ USE `enbalde` ;
 -- Table `enbalde`.`Presentaciones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`Presentaciones` (
-  `id_presentaciones` INT NOT NULL,
+  `id_presentaciones` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nombre` VARCHAR(40) NOT NULL,
   PRIMARY KEY (`id_presentaciones`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
 
+ALTER TABLE `enbalde`.`Presentaciones` AUTO_INCREMENT = 1;
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`TipoArticulos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`TipoArticulos` (
-  `id_articulos` INT NOT NULL,
+  `id_articulos` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nombre` VARCHAR(40) NOT NULL,
   PRIMARY KEY (`id_articulos`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
 
+ALTER TABLE `enbalde`.`TipoArticulos` AUTO_INCREMENT = 1;
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`Articulos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`Articulos` (
-  `id_articulos` INT NOT NULL,
+  `id_articulos` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nombre` VARCHAR(200) NOT NULL,
   `descripcion` VARCHAR(200) NOT NULL,
   `tipo` INT NOT NULL,
@@ -52,62 +52,63 @@ CREATE TABLE IF NOT EXISTS `enbalde`.`Articulos` (
   `imagen` VARCHAR(512) NOT NULL,
   `alicuota` FLOAT NULL DEFAULT NULL,
   PRIMARY KEY (`id_articulos`),
-  INDEX `fk_tipo_art_idx` (`tipo` ASC) VISIBLE,
-  INDEX `fk_presen_idx` (`presentacion` ASC) VISIBLE,
   CONSTRAINT `fk_presentacion`
     FOREIGN KEY (`presentacion`)
     REFERENCES `enbalde`.`Presentaciones` (`id_presentaciones`),
   CONSTRAINT `fk_tipo_articulo`
     FOREIGN KEY (`tipo`)
     REFERENCES `enbalde`.`TipoArticulos` (`id_articulos`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`Articulos` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`TipoComprobantes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`TipoComprobantes` (
-  `id_tipo_comprobantes` INT NOT NULL,
+  `id_tipo_comprobantes` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nombre` VARCHAR(40) NOT NULL,
   PRIMARY KEY (`id_tipo_comprobantes`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`TipoComprobantes` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`Comprobantes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`Comprobantes` (
-  `id_comprobantes` CHAR(3) NOT NULL,
+  `id_comprobantes` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `descripcion` VARCHAR(60) NOT NULL,
   `tipo` INT NOT NULL,
   `signo` CHAR(1) NOT NULL,
   PRIMARY KEY (`id_comprobantes`),
-  INDEX `fk_tipo_compro_idx` (`tipo` ASC) VISIBLE,
   CONSTRAINT `fk_tipo_comprobante`
     FOREIGN KEY (`tipo`)
     REFERENCES `enbalde`.`TipoComprobantes` (`id_tipo_comprobantes`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`Comprobantes` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`RegimenIvas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`RegimenIvas` (
-  `id_regimen_iva` INT NOT NULL,
+  `id_regimen_iva` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nombre` VARCHAR(40) NOT NULL,
   PRIMARY KEY (`id_regimen_iva`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`RegimenIvas` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`Usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`Usuarios` (
-  `id_usuarios` INT NOT NULL,
+  `id_usuarios` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `tipo_usuario` INT NOT NULL,
   `nombre` VARCHAR(40) NOT NULL,
   `apellido` VARCHAR(40) NOT NULL,
@@ -118,33 +119,34 @@ CREATE TABLE IF NOT EXISTS `enbalde`.`Usuarios` (
   `pasword` VARCHAR(40) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_usuarios`),
-  INDEX `fk_regiva_idx` (`regIva` ASC) VISIBLE,
   CONSTRAINT `fk_regiva`
     FOREIGN KEY (`regIva`)
     REFERENCES `enbalde`.`RegimenIvas` (`id_regimen_iva`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`Usuarios` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`Envios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`Envios` (
-  `id_envios` INT NOT NULL,
+  `id_envios` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nombre` VARCHAR(40) NOT NULL,
   `monto` FLOAT NULL DEFAULT NULL,
   PRIMARY KEY (`id_envios`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`Envios` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`MovComprobantes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`MovComprobantes` (
-  `id_mov_coprob` INT NOT NULL,
+  `id_mov_coprob` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `nrocomprobante` CHAR(14) NOT NULL,
-  `comprobante` CHAR(3) NOT NULL,
+  `comprobante` INT NOT NULL,
   `fecha` DATE NOT NULL,
   `usuario` INT NOT NULL,
   `neto` DOUBLE NULL DEFAULT NULL,
@@ -154,9 +156,6 @@ CREATE TABLE IF NOT EXISTS `enbalde`.`MovComprobantes` (
   `total` DOUBLE NULL DEFAULT NULL,
   `envio` INT NOT NULL,
   PRIMARY KEY (`id_mov_coprob`),
-  INDEX `fk_compro_idx` (`comprobante` ASC) VISIBLE,
-  INDEX `fk_entidad_idx` (`usuario` ASC) VISIBLE,
-  INDEX `fk_envio_idx` (`envio` ASC) VISIBLE,
   CONSTRAINT `fk_comprobante`
     FOREIGN KEY (`comprobante`)
     REFERENCES `enbalde`.`Comprobantes` (`id_comprobantes`),
@@ -166,50 +165,51 @@ CREATE TABLE IF NOT EXISTS `enbalde`.`MovComprobantes` (
   CONSTRAINT `fk_envio`
     FOREIGN KEY (`envio`)
     REFERENCES `enbalde`.`Envios` (`id_envios`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`MovComprobantes` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`DetMovComprobantes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`DetMovComprobantes` (
-  `id_det_mov_comrpro` INT NOT NULL,
+  `id_det_mov_compro` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `id_mov` INT NOT NULL,
   `articulo` INT NOT NULL,
   `cantidad` INT NOT NULL,
   `pre_unitario` DOUBLE NULL DEFAULT NULL,
   `alicuo` FLOAT NULL DEFAULT NULL,
-  PRIMARY KEY (`id_det_mov_comrpro`),
-  INDEX `fk_articulo_idx` (`articulo` ASC) VISIBLE,
-  INDEX `fk_movCompro_idx` (`id_mov` ASC) VISIBLE,
+  PRIMARY KEY (`id_det_mov_compro`),
   CONSTRAINT `fk_articulo`
     FOREIGN KEY (`articulo`)
     REFERENCES `enbalde`.`Articulos` (`id_articulos`),
   CONSTRAINT `fk_movComprobante`
     FOREIGN KEY (`id_mov`)
     REFERENCES `enbalde`.`MovComprobantes` (`id_mov_coprob`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`DetMovComprobantes` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`Ofertas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`Ofertas` (
-  `id_ofertas` INT NOT NULL,
+  `id_ofertas` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `porcentaje` FLOAT NOT NULL,
   `fecha_vencimiento` DATE NOT NULL,
   PRIMARY KEY (`id_ofertas`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`Ofertas` AUTO_INCREMENT = 1;
 
 
 -- -----------------------------------------------------
 -- Table `enbalde`.`OfertaArticulo`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `enbalde`.`OfertaArticulo` (
-  `id_ofertaarticulos` INT NOT NULL,
+  `id_ofertaarticulos` INT NOT NULL UNIQUE AUTO_INCREMENT,
   `id_articulos` INT NOT NULL,
   `id_ofertas` INT NOT NULL,
   PRIMARY KEY (`id_ofertaarticulos`),
@@ -219,8 +219,9 @@ CREATE TABLE IF NOT EXISTS `enbalde`.`OfertaArticulo` (
   CONSTRAINT `fk_ofertaarticulo_oferta`
     FOREIGN KEY (`id_ofertas`)
     REFERENCES `enbalde`.`Ofertas` (`id_ofertas`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+ENGINE = InnoDB;
+
+ALTER TABLE `enbalde`.`OfertaArticulo` AUTO_INCREMENT = 1;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
