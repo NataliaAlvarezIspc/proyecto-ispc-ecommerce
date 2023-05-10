5281dc30487f8487d96287f83b8648e8b74c7aea
Roberto <rpgrca@gmail.com>
2023-05-09 23:29:18 -0300

    agregar servicio de productos

--- front-end/enbalde/src/app/pages/catalogo/catalogo.component.ts
+++ front-end/enbalde/src/app/pages/catalogo/catalogo.component.ts
@@ -1,27 +1,27 @@
 import { Component, Input } from '@angular/core';
-import { Producto, ProductoClass } from '../producto/modelo/modelo.producto';
+import { Producto } from '../producto/modelo/modelo.producto';
+import { ProductosService } from 'src/app/productos.service';
 
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
-  styleUrls: ['./catalogo.component.css']
+  styleUrls: ['./catalogo.component.css'],
+  providers: [ ProductosService ] // declaramos un proveedor de servicio,
+                                  // ProductosService
 })
 
 export class CatalogComponent {
   carrito: Producto[] = [];
+  @Input() productos: Producto [] = [];
 
-  @Input() productos: Producto [] = [
-    new ProductoClass(1, "Tentate Chocolate", "Helado sabor chocolate artesanal", 1100, 7, "/assets/img/chocolate.jpg"),
-    new ProductoClass(2, "Tentate Frutilla", "Helado sabor frutilla artesanal", 1100, 8, "/assets/img/chocolate.jpg"),
-    new ProductoClass(3, "Tentate Dulce de Leche", "Helado sabor dulce de leche artesanal", 1100, 8, "/assets/img/chocolate.jpg"),
-    new ProductoClass(4, "Tentate Choco-frutilla", "Helado sabor chocolate y frutilla artesanal", 1200, 7,"/assets/img/chocolate.jpg"),
-    new ProductoClass(5, "Tentate frutilla-Americana", "Helado sabor frutilla y crema americana artesanal", 1200, 8, "/assets/img/chocolate.jpg"),
-    new ProductoClass(6, "Tentate D.Leche-Argentino", "Helado  D.Leche-Argentino y chocolate artesanal", 1200, 4, "/assets/img/chocolate.jpg"),
-    new ProductoClass(7, "Tentate Menta granizada", "Helado Menta granizada y chocolate artesanal", 1200, 4, "/assets/img/chocolate.jpg"),
-    new ProductoClass(8, "Tentate Frutos del bosque", "Helado Frutos del bosque y frutillaartesanal", 1200, 4, "/assets/img/chocolate.jpg")
+  constructor(public productosService: ProductosService) {
+  }
 
-  ];
-//Acomodé los ID y agg las img, junto con la funcion de agregarAlCarrito();
+  ngOnInit() : void {
+    this.productos = this.productosService.obtenerProductos();
+  }
+
+  //Acomodé los ID y agg las img, junto con la funcion de agregarAlCarrito();
   agregarAlCarrito(producto: Producto) {
     if (producto.cantidadDisponible > 0) {
       producto.cantidadDisponible--;
