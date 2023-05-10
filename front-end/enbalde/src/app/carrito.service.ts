e5805bc0a3ca8b3835cdc9449c79d4327964d650
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-10 18:23:20 -0300

    [ADD] Servicio de carrito

---
+++ front-end/enbalde/src/app/carrito.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ProductoClass } from './pages/producto/modelo/modelo.producto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarritoService {
+
+  constructor() { }
+
+  obtenerProductosCarrito(){
+    return[
+    new ProductoClass(1, "Tentate Chocolate", "Helado sabor chocolate artesanal", 1100, 7, "/assets/img/chocolate.jpg"),
+    new ProductoClass(2, "Tentate Frutilla", "Helado sabor frutilla artesanal", 1100, 8, "/assets/img/chocolate.jpg")
+  ]
+  }
+  
+}
