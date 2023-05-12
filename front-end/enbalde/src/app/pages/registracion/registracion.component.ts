130a031d2014659f5a2cd2a7fdc228f52081ccb4
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-05-12 00:26:55 -0300

    subo form registro reactivo, falta validaciones

--- front-end/enbalde/src/app/pages/registracion/registracion.component.ts
+++ front-end/enbalde/src/app/pages/registracion/registracion.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 
@@ -8,25 +8,34 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './registracion.component.html',
   styleUrls: ['./registracion.component.css']
 })
-export class RegistracionComponent {
-  registrarForm = new FormGroup({
-    name: new FormControl(''),
-    lname: new FormControl(''),
-    mail: new FormControl(''),
-    adress: new FormControl(''),
-    user: new FormControl(''),
-    password: new FormControl(''),
-    phone: new FormControl('')
-    
+
+export class RegistracionComponent implements OnInit {
+
+  registrarForm = this.fb.group({
+    fname:['',[Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
+    lname: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
+    mail: ['', [Validators.required, Validators.minLength(10), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'), Validators.maxLength(45)]],
+    adress: ['', [Validators.required, Validators.maxLength(40)]],
+    user: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+    password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+    phone: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(25)]],
   });
 
-  updateProfile() {
+  updateProfile(): void {
     this.registrarForm.patchValue({
-      name: '',
+      fname: '',
       lname: '',
       adress: '',
       phone: ''
     });
+  }
+
+  constructor (private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSave() {
+    console.log('Form->' + JSON.stringify(this.registrarForm.value));
   }
 
 }
