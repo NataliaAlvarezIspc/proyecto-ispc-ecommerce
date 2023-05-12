8c730971d168a8c69f62d5c5c87ebc781151df1a
Roberto <rpgrca@gmail.com>
2023-05-12 19:00:37 -0300

    consolidar espaciado

--- front-end/enbalde/src/app/carrito.service.ts
+++ front-end/enbalde/src/app/carrito.service.ts
@@ -8,11 +8,10 @@ export class CarritoService {
 
   constructor() { }
 
-  obtenerProductosCarrito(){
-    return[
-    new ProductoClass(1, "Tentate Chocolate", "Helado sabor chocolate artesanal", 1100, 7, "/assets/img/chocolate.jpg"),
-    new ProductoClass(2, "Tentate Frutilla", "Helado sabor frutilla artesanal", 1100, 8, "/assets/img/chocolate.jpg")
-  ]
+  obtenerProductosCarrito() {
+    return [
+      new ProductoClass(1, "Tentate Chocolate", "Helado sabor chocolate artesanal", 1100, 7, "/assets/img/chocolate.jpg"),
+      new ProductoClass(2, "Tentate Frutilla", "Helado sabor frutilla artesanal", 1100, 8, "/assets/img/chocolate.jpg")
+    ];
   }
-  
 }
