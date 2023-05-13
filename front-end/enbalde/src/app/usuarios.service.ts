a8a5fae9619f3c4ffb4f9443ba4a547d6d51e53e
Roberto <rpgrca@gmail.com>
2023-05-13 15:09:14 -0300

    declarar boolean como retorno, limpieza

--- front-end/enbalde/src/app/usuarios.service.ts
+++ front-end/enbalde/src/app/usuarios.service.ts
@@ -3,26 +3,25 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class UsuariosService {
 
+export class UsuariosService {
   constructor() { }
 
-  registrar(fname: string|null|undefined, lname: string|null|undefined, mail: string|null|undefined, adress: string|null|undefined, user: string|null|undefined , password: string|null|undefined, phone: string|null|undefined) {
+  registrar(fname: string|null|undefined, lname: string|null|undefined, mail: string|null|undefined, adress: string|null|undefined, user: string|null|undefined , password: string|null|undefined, phone: string|null|undefined): boolean {
     if (user== 'Natalia' && password == '123456')
       return false
     else
       return true
   }
 
-  login(user: string|null|undefined , password: string|null|undefined){
+  login(user: string|null|undefined , password: string|null|undefined): boolean {
     if (user== 'Natalia' && password == '123456')
       return true
-    else 
+    else
       return false
-
   }
 
-  restablecerClave(mail: string|null|undefined){
-    return []
+  restablecerClave(mail: string|null|undefined): boolean {
+    return true
   }
 }
