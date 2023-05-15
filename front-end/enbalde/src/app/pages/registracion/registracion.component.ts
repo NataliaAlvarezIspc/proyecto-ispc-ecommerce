9d6d30bfc8fc0c89e1299ab1395a6e20d765baae
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-05-15 01:18:25 -0300

    agregando validaciones a registración

--- front-end/enbalde/src/app/pages/registracion/registracion.component.ts
+++ front-end/enbalde/src/app/pages/registracion/registracion.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuariosService } from 'src/app/usuarios.service'; 
 
 
@@ -12,18 +12,26 @@ import { UsuariosService } from 'src/app/usuarios.service';
 })
 
 export class RegistracionComponent implements OnInit {
+  registrarForm!: FormGroup
+  usuarios= { fname: '', lname:'', mail:'', adress:'', user:'', password:'', phone:'' }
 
-  registrarForm = this.fb.group({
-    fname:['',[Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
-    lname: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-    mail: ['', [Validators.required, Validators.minLength(10), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'), Validators.maxLength(45)]],
-    adress: ['', [Validators.required, Validators.maxLength(40)]],
-    user: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
-    password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
-    phone: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(25)]],
-  });
+  constructor (private fb: FormBuilder, private usuariosService: UsuariosService) {}
+  
+  
+  ngOnInit(): void {
+    this.registrarForm = this.fb.group({
+      fname:[this.usuarios.fname,[Validators.required, Validators.minLength(4), Validators.maxLength(40)]],
+      lname: [this.usuarios.lname,[Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
+      mail: [this.usuarios.mail, [Validators.required, Validators.minLength(10), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'), Validators.maxLength(45)]],
+      adress: [this.usuarios.adress, [Validators.required, Validators.maxLength(40)]],
+      user: [this.usuarios.user, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      password: [this.usuarios.password, [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+      phone: [this.usuarios.phone, [Validators.required, Validators.minLength(8), Validators.maxLength(25)]],
+    });
 
-  updateProfile(): void {
+  }
+
+  updateProfile() {
     this.registrarForm.patchValue({
       fname: '',
       lname: '',
@@ -31,13 +39,16 @@ export class RegistracionComponent implements OnInit {
       phone: ''
     });
   }
-
-  constructor (private fb: FormBuilder, private usuariosService: UsuariosService) {}
-
-  ngOnInit(): void {
+  
+  get fname() { return this.registrarForm.get('fname');}
+  get lname() { return this.registrarForm.get('lname');}
+  get mail() { return this.registrarForm.get('mail');}
+  get adress() { return this.registrarForm.get('adress');}
+  get user() { return this.registrarForm.get('user');}
+  get password() { return this.registrarForm.get('password');}
+  get phone() { return this.registrarForm.get('phone');}
     
-  }
-
+  
   onSubmit() {
     if (this.usuariosService.registrar(this.registrarForm.value.fname, this.registrarForm.value.lname, this.registrarForm.value.mail, this.registrarForm.value.adress, this.registrarForm.value.user, this.registrarForm.value.password, this.registrarForm.value.phone))
       alert('Su registración fue creada con éxito')
