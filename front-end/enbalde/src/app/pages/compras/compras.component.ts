d1fd2642092a6dd59e8dac9068c91ab2f615da03
Brian Almada <brianalexisalmada84@gmail.com>
2023-05-15 23:35:25 -0300

    Importar la clase 'CompraClass' en compras.component.ts y crear array vacio de compras

--- front-end/enbalde/src/app/pages/compras/compras.component.ts
+++ front-end/enbalde/src/app/pages/compras/compras.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Compra, CompraClass } from './modelo/modelo.compra';
 
 @Component({
   selector: 'app-compras',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./compras.component.css']
 })
 export class ComprasComponent {
-
+  @Input() compras: Compra [] = []
 }
