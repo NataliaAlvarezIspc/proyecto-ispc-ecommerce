5b46b9fc95d3055cdcc8f47c6b7cbeacb190e86e
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-05-16 20:40:52 -0300

    agregando validaciones y otras funciones comentadas para trabajar mas adelante

--- front-end/enbalde/src/app/pages/perfil/perfil.component.ts
+++ front-end/enbalde/src/app/pages/perfil/perfil.component.ts
@@ -7,23 +7,70 @@ import { Router } from '@angular/router';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
- 
+  perfilForm!: FormGroup;
+  perfil!: { mail: ''; adress: ''; password: ''; phone: ''; };
+  mensaje: string = '';
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
   fname: string = 'Natalia';
   lname: string = 'García';
-  mail: string = 'nataliagarcia@gmail.com';
-  adress: string = 'Av. Siempre Viva 2233';
+  mailB: string = 'nataliagarcia@gmail.com';
+  adressB: string = 'Av. Siempre Viva 2233';
   user: string = 'Natalia';
-  password: string = '123456';
-  phone: string = '112222222';
+  passwordB: string = '123456';
+  phoneB: string = '112222222';
   
-  constructor(private router: Router) {}
+  
 
   ngOnInit(): void {
+//    this.obtenerPerfil();
+//    this.crearFormulario();
+      this.perfilForm = this.fb.group ({
+        mail: [this.perfil.mail, [Validators.required, Validators.minLength(10), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')]],
+        adress: [this.perfil.adress, [Validators.required, Validators.maxLength(40)]],
+        password: [this.perfil.password, [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+        phone: [this.perfil.phone, [Validators.required, Validators.minLength(8), Validators.maxLength(25)]],
+        });
   }
+//  obtenerPerfil() {
+ //   this.perfilService.obtenerPerfil().suscribe(
+//      (data) => {
+//       this.perfil = data;
+//      },
+//      (error) => {
+//        console.error(error);
+//      }
+//    );
+//  }
 
-  onSubmit() {
+//  crearFormulario(): void {
+//    this.perfilForm = this.fb.group ({
+//      mail: [this.perfil.mail, [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')]],
+//      password: [this.perfil.password, Validators.required],
+//      adress: [this.perfil.adress, Validators.required],
+//      phone: [this.perfil.phone, Validators.required],
+//
+//    })
+// }
+  get mail() { return this.perfilForm.get('mail'); }
+  get adress() { return this.perfilForm.get('adress'); }
+  get password() { return this.perfilForm.get('password'); }
+  get phone() { return this.perfilForm.get('phone'); }
+
+  onSubmit(): void {
+  //  if (this.perfilForm.valid) {
+  //    this.perfilService.actualizarPerfil(this.perfilForm.value).suscribe(
+  //      (data) => {
+  //        this.perfil = data;
+  //        this.mensaje = 'Los cambios han sido guardados correctamente.';
+  //      },
+  //      (error) => {
+  //        console.error(error);
+  //      }
+  //    );
+  //  }
     this.router.navigate(['/']);
   }
-
 }
 
