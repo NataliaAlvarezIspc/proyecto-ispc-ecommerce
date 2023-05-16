931a618f41f441ae89184a9ac8402b116373a9ea
Brian Almada <brianalexisalmada84@gmail.com>
2023-05-16 01:29:31 -0300

    Volver al codigo de una sola tabla de compras

--- front-end/enbalde/src/app/pages/compras/compras.component.ts
+++ front-end/enbalde/src/app/pages/compras/compras.component.ts
@@ -7,43 +7,7 @@ import { Compra, CompraClass } from './modelo/modelo.compra';
   styleUrls: ['./compras.component.css']
 })
 export class ComprasComponent {
-  @Input() compras: Compra [] = [];
-
-  todasCompras: Compra[] = [
-    new CompraClass(new Date("01/30/2022").getTime(), [ "1 x Helado" ], 1000),
-    new CompraClass(new Date("05/15/2022").getTime(), [ "1 x Helado" ], 1000),
-    new CompraClass(new Date("01/30/2023").getTime(), [ "1 x Helado" ], 1000)
+  @Input() compras: Compra [] = [
+    new CompraClass(new Date("01/12/2022").getTime(), [ "1 x Helado" ], 1000)
   ];
-  comprasUltimoMes: Compra[] = [
-    new CompraClass(new Date("01/30/2022").getTime(), [ "1 x Helado" ], 1000),
-    new CompraClass(new Date("05/15/2022").getTime(), [ "1 x Helado" ], 1000),
-    new CompraClass(new Date("01/30/2023").getTime(), [ "1 x Helado" ], 1000)
-  ];
-  ultimaCompra: Compra | undefined;
-
-  ngOnChanges() {
-    this.todasCompras = this.compras;
-    this.comprasUltimoMes = this.obtenerComprasUltimoMes();
-    this.ultimaCompra = this.obtenerUltimaCompra();
-  }
-
-  obtenerComprasUltimoMes(): Compra[] {
-    const fechaActual = new Date();
-    const unMesAtras = new Date();
-    unMesAtras.setMonth(unMesAtras.getMonth() - 1);
-
-    return this.compras.filter(compra => {
-      const fechaCompra = new Date(compra.fecha);
-      return fechaCompra >= unMesAtras && fechaCompra <= fechaActual;
-    });
-  }
-
-  obtenerUltimaCompra(): Compra | undefined {
-    return this.compras.reduce((ultimaCompra: Compra | undefined, compra: Compra) => {
-      if (!ultimaCompra || compra.fecha > ultimaCompra.fecha) {
-        return compra;
-      }
-      return ultimaCompra;
-    }, undefined);
-  }
 }
