fbea432f1f5a9b7fe67ec40e12b93ce98475e216
Roberto <rpgrca@gmail.com>
2023-05-17 11:08:37 -0300

    agregar formulario para crear productos en dashboard

--- front-end/enbalde/src/app/pages/dashboard/dashboard.component.ts
+++ front-end/enbalde/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Producto, ProductoClass } from '../producto/modelo/modelo.producto';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProductosService } from 'src/app/productos.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,16 +9,25 @@ import { Producto, ProductoClass } from '../producto/modelo/modelo.producto';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  @Input() productos: Producto [] = [
-    new ProductoClass(1, "Tentate Chocolate", "Helado sabor chocolate artesanal", 1100, 7, "/assets/img/chocolate.jpg"),
-    new ProductoClass(2, "Tentate Frutilla", "Helado sabor frutilla artesanal", 1100, 8, "/assets/img/chocolate.jpg"),
-    new ProductoClass(3, "Tentate Dulce de Leche", "Helado sabor dulce de leche artesanal", 1100, 8, "/assets/img/chocolate.jpg"),
-    new ProductoClass(4, "Tentate Choco-frutilla", "Helado sabor chocolate y frutilla artesanal", 1200, 7,"/assets/img/chocolate.jpg"),
-    new ProductoClass(5, "Tentate frutilla-Americana", "Helado sabor frutilla y crema americana artesanal", 1200, 8, "/assets/img/chocolate.jpg"),
-    new ProductoClass(6, "Tentate D.Leche-Argentino", "Helado  D.Leche-Argentino y chocolate artesanal", 1200, 4, "/assets/img/chocolate.jpg"),
-    new ProductoClass(7, "Tentate Menta granizada", "Helado Menta granizada y chocolate artesanal", 1200, 4, "/assets/img/chocolate.jpg"),
-    new ProductoClass(8, "Tentate Frutos del bosque", "Helado Frutos del bosque y frutillaartesanal", 1200, 4, "/assets/img/chocolate.jpg")
-  ];
+  crearProductoForm!: FormGroup;
+
+  @Input() productos!: Producto [];
+
+  constructor(private formBuilder: FormBuilder, private productosService: ProductosService) {
+    this.productos = this.productosService.obtenerProductos();
+  }
+
+  ngOnInit(): void {
+    this.crearProductoForm = this.formBuilder.group({
+      nombre: ["", [Validators.required, Validators.minLength(1), Validators.maxLength(40)]],
+      descripcion: ["", [Validators.required, Validators.minLength(1), Validators.maxLength(40)]],
+      tipo: ["", [Validators.required]],
+      precio: [0, [Validators.required, Validators.min(0)]],
+      costo: [0, [Validators.required, Validators.min(0)]],
+      alicuota: [0, [Validators.required, Validators.min(0)]],
+      imagen: [""]
+    });
+  }
 
   editar(producto: Producto) {
     alert(`Editando ${producto.titulo} (próximamente)`);
@@ -26,7 +37,12 @@ export class DashboardComponent {
     alert(`Borrando ${producto.titulo} (próximamente)`);
   }
 
-  crear() {
-    alert("Creando artículo nuevo (próximamente)");
+  crear(value: any) {
+    if (this.productosService.crearProducto(value.nombre, value.descripcion, value.tipo, value.precio, value.costo, value.alicuota, value.imagen)) {
+      alert(`Artículo ${value.nombre} creado correctamente`);
+    }
+    else {
+      alert(`Error creando artículo ${value.nombre}`);
+    }
   }
 }
