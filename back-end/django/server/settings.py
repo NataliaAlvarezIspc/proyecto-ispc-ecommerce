4745ac342a244ba67e069326aa8b2b9d110cbc48
Roberto <rpgrca@gmail.com>
2023-05-17 15:42:18 -0300

    agregar enablde como app instalada en django

--- back-end/django/server/settings.py
+++ back-end/django/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'enbalde'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
+        'NAME': 'enbalde',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
