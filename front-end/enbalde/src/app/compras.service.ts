135c7f68fce674dc03e044a94682f0f9db48c62e
Brian Almada <brianalexisalmada84@gmail.com>
2023-05-17 19:25:08 -0300

    Crear metodo 'obtenerCompras', rellenar con hardcode e importar 'compraClass'

--- front-end/enbalde/src/app/compras.service.ts
+++ front-end/enbalde/src/app/compras.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CompraClass } from './pages/compras/modelo/modelo.compra';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,9 @@ import { Injectable } from '@angular/core';
 export class ComprasService {
 
   constructor() { }
+
+  obtenerCompras () {
+    return [new CompraClass(new Date("01/12/2022").getTime(), [ "1 x Helado" ], 1000)]
+  }
+
 }
