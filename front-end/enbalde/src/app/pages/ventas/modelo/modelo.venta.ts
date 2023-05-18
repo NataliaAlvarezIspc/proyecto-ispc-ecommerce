b735a2db13e4394a0c845ec414898d153ba27b98
Roberto <rpgrca@gmail.com>
2023-05-18 18:03:11 -0300

    agregar json para compras

--- front-end/enbalde/src/app/pages/ventas/modelo/modelo.venta.ts
+++ front-end/enbalde/src/app/pages/ventas/modelo/modelo.venta.ts
@@ -2,18 +2,21 @@ import { Seleccion } from "../../carrito/modelo/modelo.seleccion";
 
 export interface Venta {
   comprador: string;
+  fecha: Date,
   selecciones: Seleccion[];
   total: number;
 }
 
 export class VentaClass implements Venta {
-  constructor(comprador: string, selecciones: Seleccion[], total: number) {
+  constructor(comprador: string, fecha: Date, selecciones: Seleccion[], total: number) {
     this.comprador = comprador;
+    this.fecha = fecha;
     this.selecciones = selecciones;
     this.total = total;
   }
 
   comprador: string;
+  fecha: Date;
   selecciones: Seleccion[];
   total: number;
 }
