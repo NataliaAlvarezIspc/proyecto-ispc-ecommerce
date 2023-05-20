604a14e05bd36574d5860e3bc7d15cb11326ae4b
Roberto <rpgrca@gmail.com>
2023-05-20 19:13:33 -0300

    agregar simple test, ajustar pipeline

--- back-end/django/enbalde/tests.py
+++ back-end/django/enbalde/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from enbalde.models import Envio
 
 # Create your tests here.
+
+class EnvioTestCase(TestCase):
+    def test_envio_se_inicializa_correctamente(self):
+        sut = Envio.objects.create(id=3, nombre="Retiro en tienda", monto=0)
+        self.assertEqual(3, sut.id)
+        self.assertEqual("Retiro en tienda", sut.nombre)
+        self.assertEqual(0, sut.monto)
