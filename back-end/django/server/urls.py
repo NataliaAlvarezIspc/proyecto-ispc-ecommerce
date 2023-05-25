3ecad417a0c3acb274b32d8f9c8ec1d9c2381daa
Roberto <rpgrca@gmail.com>
2023-05-24 23:04:57 -0300

    agregar viewset para Usuario

--- back-end/django/server/urls.py
+++ back-end/django/server/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+from enbalde.models import Usuario
+
+class UsuarioSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['url', 'username', 'tipo', 'first_name', 'last_name', 'email', 'direccion', 'telefono', 'observaciones']
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+router = routers.DefaultRouter()
+router.register(r'usuarios', UsuarioViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
