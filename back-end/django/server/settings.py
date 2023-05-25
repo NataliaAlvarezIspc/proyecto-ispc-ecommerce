d3657c85ee06504fe8f44b5a167e8e5de6600775
Roberto <rpgrca@gmail.com>
2023-05-24 21:54:35 -0300

    agregar rest_framework como aplicacion

--- back-end/django/server/settings.py
+++ back-end/django/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'enbalde'
 ]
 
@@ -78,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enbalde',
         'USER': 'root',
-        'PASSWORD': 'braianmysql',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
