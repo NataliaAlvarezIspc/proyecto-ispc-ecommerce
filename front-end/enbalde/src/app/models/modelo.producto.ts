eae29e81eae7f1633f2b1738bda2c05222d3e53c
Roberto <rpgrca@gmail.com>
2023-05-27 17:11:07 -0300

    agregar producto nulo

--- front-end/enbalde/src/app/models/modelo.producto.ts
+++ front-end/enbalde/src/app/models/modelo.producto.ts
@@ -1,3 +1,4 @@
+import { TipoProductoClass } from "./modelo.tipoProducto";
 import { TipoProducto } from "./modelo.tipoProducto";
 
 export interface Producto {
@@ -9,4 +10,28 @@ export interface Producto {
   cantidad: number;
   imagen: string;
   tipo: TipoProducto;
+}
+
+export class ProductoClass implements Producto {
+  constructor(id: number, nombre: string, descripcion: string, precio: number, costo: number, cantidad: number, imagen: string, tipo: TipoProducto) {
+    this.id = id;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.costo = costo;
+    this.cantidad = cantidad;
+    this.imagen = imagen;
+    this.tipo = tipo;
+  }
+
+  id: number;
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  costo: number;
+  cantidad: number;
+  imagen: string;
+  tipo: TipoProducto;
+
+  static Nulo: Producto = new ProductoClass(-1, "", "", 0, 0, 0, "", TipoProductoClass.Nulo);
 }
