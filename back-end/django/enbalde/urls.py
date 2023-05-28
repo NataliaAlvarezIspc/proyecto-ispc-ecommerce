4364447deea578d46419301a068fff5477bee803
Roberto <rpgrca@gmail.com>
2023-05-28 20:37:00 -0300

    una sola linea

--- back-end/django/enbalde/urls.py
+++ back-end/django/enbalde/urls.py
@@ -2,12 +2,7 @@ from django.urls import path, include
 from .views.usuario_views import LoginView, LogoutView, SignupView
 
 urlpatterns = [
-    path('auth/login/',
-         LoginView.as_view(), name='auth_login'),
-
-    path('auth/logout/',
-         LogoutView.as_view(), name='auth_logout'),
-
-    path('auth/signup/',
-         SignupView.as_view(), name='auth_signup'),
+    path('auth/login/', LoginView.as_view(), name='auth_login'),
+    path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
+    path('auth/signup/', SignupView.as_view(), name='auth_signup'),
 ]
