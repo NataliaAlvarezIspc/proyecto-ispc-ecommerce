d58c2a04354288ce5d1f13d64f4d64d3b4f1c7e7
Roberto <rpgrca@gmail.com>
2023-05-27 22:57:04 -0300

    mover generar nombre unico a common

--- back-end/django/enbalde/views/common.py
+++ back-end/django/enbalde/views/common.py
@@ -1,5 +1,12 @@
+import os
+import uuid
 from django.http import JsonResponse
 from rest_framework import status
+
+
+def generar_nombre_unico(nombre):
+    return f"{uuid.uuid4().hex}{os.path.splitext(nombre)[1]}"
+
 
 def crear_respuesta(mensaje: str, data: any = None, status_code: status = status.HTTP_200_OK):
     return JsonResponse({ "mensaje": mensaje, "data": data, "status": status_code }, status=status_code, safe=False)
