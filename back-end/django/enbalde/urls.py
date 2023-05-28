eabe21c0679a9e70d851c7e4bb47c9ec7393cff5
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-05-27 23:52:55 -0300

    agregando vista a urls de la app

--- back-end/django/enbalde/urls.py
+++ back-end/django/enbalde/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views.usuario_views import LoginView, LogoutView
+from .views.usuario_views import LoginView, LogoutView, SignupView
 
 urlpatterns = [
     path('auth/login/',
@@ -7,4 +7,7 @@ urlpatterns = [
 
     path('auth/logout/',
          LogoutView.as_view(), name='auth_logout'),
+
+    path('auth/signup/',
+         SignupView.as_view(), name='auth_signup'),
 ]
