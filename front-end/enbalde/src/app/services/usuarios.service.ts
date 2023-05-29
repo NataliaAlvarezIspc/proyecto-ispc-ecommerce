2cad383eef86a91764657742c959b57dd3c0a05b
Roberto <rpgrca@gmail.com>
2023-05-28 22:07:20 -0300

    agregar registracion al back-end

--- front-end/enbalde/src/app/services/usuarios.service.ts
+++ front-end/enbalde/src/app/services/usuarios.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Usuario } from '../models/modelo.usuario';
+import { Observable, catchError, throwError } from 'rxjs';
+import { TipoUsuario, Usuario } from '../models/modelo.usuario';
+import { ResultadoApi } from '../models/modelo.resultado';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UsuariosService {
-  private usuariosUrl: string = 'assets/usuarios.json';
+  private usuariosUrl: string = 'http://localhost:8000/api/usuarios';
 
   constructor(private http: HttpClient) {
   }
 
-  registrar(nombre: string, apellido: string, email: string, direccion: string, usuario: string, clave: string, telefono: string): boolean {
-    if (usuario == 'Natalia')
-      return false
-    else
-      return true
+  registrar(nombre: string, apellido: string, email: string, direccion: string, usuario: string, clave: string, telefono: string, tipo: TipoUsuario): Observable<ResultadoApi> {
+    const formData = new FormData();
+    formData.append('first_name', nombre);
+    formData.append('last_name', apellido);
+    formData.append('email', email);
+    formData.append('direccion', direccion);
+    formData.append('username', usuario);
+    formData.append('password', clave);
+    formData.append('telefono', telefono);
+    formData.append('tipo', tipo.toString())
+
+    return this.http.post<ResultadoApi>(this.usuariosUrl, formData)
+      .pipe(catchError(error => {
+        const resultado: ResultadoApi = {
+          mensaje: error.error.mensaje,
+          data: error.error.data,
+          status: error.error.status
+        };
+
+        return throwError(() => resultado);
+      }));
   }
 
   login(usuario: string, clave: string): boolean {
