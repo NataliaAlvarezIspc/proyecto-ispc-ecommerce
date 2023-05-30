d152ea0b948a019b83200312ceead446a0f9df81
Roberto <rpgrca@gmail.com>
2023-05-30 00:45:53 -0300

    implementar jwt en angular

---
+++ front-end/enbalde/src/app/services/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
