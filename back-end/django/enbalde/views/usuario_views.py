376ad9e60dbc714badf72ace16269ad7f9776e0d
Roberto <rpgrca@gmail.com>
2023-05-29 23:07:29 -0300

    wrap retorno de registracion en nuestro contenedor

--- back-end/django/enbalde/views/usuario_views.py
+++ back-end/django/enbalde/views/usuario_views.py
@@ -1,14 +1,28 @@
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import generics, status
+from rest_framework.serializers import ValidationError
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.views import APIView
 from ..serializers import UsuarioSerializer, RegistroSerializer
 from ..models import Usuario
+from ..views.common import crear_respuesta
 
 
 class SignupView(generics.CreateAPIView):
     queryset = Usuario.objects.all()
     serializer_class = RegistroSerializer
+
+    def create(self, request: Request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                self.perform_create(serializer)
+                return crear_respuesta("Usuario registrado exitosamente", serializer.data, status.HTTP_201_CREATED)
+            except ValidationError as ex:
+                return crear_respuesta(str(ex.detail[0]), status_code=status.HTTP_400_BAD_REQUEST)
+
+        return crear_respuesta("Error registrando usuario", serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(APIView):
