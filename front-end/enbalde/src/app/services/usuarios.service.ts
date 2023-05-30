2494af6511d009a57ed075b9fcfc454bd7bd94df
Roberto <rpgrca@gmail.com>
2023-05-29 23:29:57 -0300

    enviar informacion de login al servidor

--- front-end/enbalde/src/app/services/usuarios.service.ts
+++ front-end/enbalde/src/app/services/usuarios.service.ts
@@ -3,13 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { TipoUsuario, Usuario } from '../models/modelo.usuario';
 import { ResultadoApi } from '../models/modelo.resultado';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UsuariosService {
-  private usuariosUrl: string = 'http://localhost:8000/api/auth/signup/';
+  private API_URL = environment.API_URL;
+  private registracionUrl: string = `${this.API_URL}/auth/signup/`;
+  private loginUrl: string = `${this.API_URL}/auth/login/`;
+  private usuariosUrl: string = "/assets/usuarios.json";
 
   constructor(private http: HttpClient) {
   }
@@ -25,7 +29,7 @@ export class UsuariosService {
     formData.append('telefono', telefono);
     formData.append('tipo', tipo.toString())
 
-    return this.http.post<ResultadoApi>(this.usuariosUrl, formData)
+    return this.http.post<ResultadoApi>(this.registracionUrl, formData)
       .pipe(catchError(error => {
         const resultado: ResultadoApi = {
           mensaje: error.error.mensaje,
@@ -37,11 +41,21 @@ export class UsuariosService {
       }));
   }
 
-  login(usuario: string, clave: string): boolean {
-    if (usuario == 'Natalia' && clave == '123456')
-      return true
-    else
-      return false
+  login(usuario: string, clave: string): Observable<ResultadoApi> {
+    const formData = new FormData();
+    formData.append("usuario", usuario);
+    formData.append("clave", clave);
+
+    return this.http.post<ResultadoApi>(this.loginUrl, formData)
+      .pipe(catchError(error => {
+        const resultado: ResultadoApi = {
+          mensaje: error.error.mensaje,
+          data: error.error.data,
+          status: error.error.status
+        };
+
+        return throwError(() => resultado);
+      }));
   }
 
   obtenerInformacionUsuario(id: number): Observable<Usuario> {
