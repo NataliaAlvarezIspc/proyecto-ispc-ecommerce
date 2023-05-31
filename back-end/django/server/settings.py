8903a7c778420e0afeba8fff42ea1af5e646a443
Roberto <rpgrca@gmail.com>
2023-05-31 00:47:21 -0300

    ajuste

--- back-end/django/server/settings.py
+++ back-end/django/server/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enbalde',
         'USER': 'root',
-        'PASSWORD': 'gTp8xT2!',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
