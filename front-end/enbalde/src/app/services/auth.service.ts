84f2dc8fbf3bb6c2ec9e1ffcfe8799182ae2e708
Roberto <rpgrca@gmail.com>
2023-05-31 18:49:31 -0300

    probar auto deslogueo

--- front-end/enbalde/src/app/services/auth.service.ts
+++ front-end/enbalde/src/app/services/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, catchError, finalize, tap, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription, catchError, delay, finalize, of, tap, throwError } from 'rxjs';
 import { Usuario } from '../models/modelo.usuario';
 import { ResultadoApi } from '../models/modelo.resultado';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environment/environment';
 import { TokenUsuario } from './usuarios.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -18,20 +20,74 @@ export class AuthService {
 
   private autenticado$: BehaviorSubject<boolean> = new BehaviorSubject(false);
   public autenticado: Observable<boolean> = this.autenticado$.asObservable();
-  public usuario?: Usuario;
+  private usuario?: Usuario;
+  private token: any;
+  private suscripcionToken: Subscription = new Subscription()
+  private timeout: any;
 
-  constructor(private http: HttpClient) {
-    const token = localStorage.getItem('accessToken');
+  constructor(private router: Router, private http: HttpClient, private jwtHelperService: JwtHelperService) {
+    this.token = localStorage.getItem('accessToken');
     const usuarioActual = localStorage.getItem('usuarioActual');
-    if (token && usuarioActual) {
-      this.usuario = JSON.parse(usuarioActual) as Usuario;
-      this.autenticado$.next(true);
+    this.actualizarInformacionUsuario(this.token, usuarioActual);
+  }
+
+  actualizarInformacionUsuario(token: any, usuarioActual: any) {
+    if ((token != null && token != undefined) && usuarioActual) {
+      let date = this.jwtHelperService.getTokenExpirationDate(token);
+      if (date) {
+        this.token = token;
+        this.timeout = date?.valueOf() - new Date().valueOf();
+        let usuario = JSON.parse(usuarioActual) as Usuario;
+        this.autenticadoComo(usuario);
+        this.cuentaRegresivaExpiracion();
+      }
+      else {
+        this.borrarToken();
+      }
     }
+    else {
+      this.borrarToken();
+    }
+
+  }
+
+  cuentaRegresivaExpiracion() {
+    this.suscripcionToken.unsubscribe();
+    this.suscripcionToken = of(null).pipe(delay(this.timeout))
+      .subscribe((expired) => {
+        this.logout();
+        this.router.navigate(["/login"]);
+      });
   }
 
   autenticadoComo(usuarioActual: Usuario | undefined) {
     this.usuario = usuarioActual;
     this.autenticado$.next(usuarioActual != undefined);
+  }
+
+  obtenerUsuarioSiNoExpiro(): Usuario | undefined {
+    if (this.tokenExpirada(this.token)) {
+      this.borrarToken();
+      return undefined;
+    }
+
+    return this.usuario;
+  }
+
+  borrarToken(): void {
+    this.suscripcionToken.unsubscribe();
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('usuarioActual');
+    this.token = null;
+    this.autenticadoComo(undefined);
+  }
+
+  private tokenExpirada(token: string | null) {
+    if (token) {
+      const expiracion = (JSON.parse(atob(token.split('.')[1]))).exp;
+      return (Math.floor((new Date).getTime() / 1000)) >= expiracion;
+    }
+    return true;
   }
 
   login(usuario: string, clave: string): Observable<ResultadoApi> {
@@ -42,10 +98,12 @@ export class AuthService {
     return this.http.post<ResultadoApi>(this.loginUrl, formData)
       .pipe(tap((resultado: ResultadoApi) => {
         let tokenUsuario = resultado.data as TokenUsuario;
+        let usuarioActual = JSON.stringify(tokenUsuario.usuarioActual);
 
         localStorage.setItem('accessToken', `${tokenUsuario.accessToken.acceso}`);
-        localStorage.setItem('usuarioActual', JSON.stringify(tokenUsuario.usuarioActual));
-        this.autenticadoComo(tokenUsuario.usuarioActual);
+        localStorage.setItem('usuarioActual', usuarioActual);
+
+        this.actualizarInformacionUsuario(tokenUsuario.accessToken.acceso, usuarioActual)
       }),
       catchError(error => {
         const resultado: ResultadoApi = {
@@ -70,10 +128,7 @@ export class AuthService {
         return throwError(() => resultado);
       }))
       .pipe(finalize(() => {
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('usuarioActual');
-
-        this.autenticadoComo(undefined);
+        this.borrarToken();
       }));
   }
 }
