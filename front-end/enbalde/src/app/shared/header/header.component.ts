531380721a2356d356d46906a0309229008db687
Roberto <rpgrca@gmail.com>
2023-05-30 23:32:28 -0300

    ocultar  links en header dependiendo de tipo

--- front-end/enbalde/src/app/shared/header/header.component.ts
+++ front-end/enbalde/src/app/shared/header/header.component.ts
@@ -1,12 +1,16 @@
+import { HttpStatusCode } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
-import { Usuario } from 'src/app/models/modelo.usuario';
+import { Router } from '@angular/router';
+import { ResultadoApi } from 'src/app/models/modelo.resultado';
+import { Usuario, TipoUsuario } from 'src/app/models/modelo.usuario';
 import { ProductosService } from 'src/app/services/productos.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  providers: [ ProductosService]
+  providers: [ ProductosService, UsuariosService ]
 })
 
 export class HeaderComponent {
@@ -15,14 +19,24 @@ export class HeaderComponent {
   buscarResults!: any[];
   showResults: boolean = false
 
-  constructor (private productosService: ProductosService) {
+  constructor (private productosService: ProductosService, private usuariosService: UsuariosService, private router: Router) {
+    this.usuariosService.UsuarioIngresando
+      .subscribe((u: Usuario) => {
+        this.usuario = u;
+      });
   }
 
   ngOnInit(): void {
     let token = localStorage.getItem('accessToken');
     let usuarioActual = localStorage.getItem('usuarioActual');
+    console.log(usuarioActual)
     if (usuarioActual) {
+      console.log(1)
       this.usuario = JSON.parse(usuarioActual) as Usuario;
+    }
+    else {
+      console.log(2)
+      this.usuario = undefined;
     }
   }
 
@@ -36,4 +50,20 @@ export class HeaderComponent {
   limpiar() {
     this.showResults = false;
   }
+
+  logout() {
+    this.usuariosService.logout()
+      .subscribe((resultado: ResultadoApi) => {
+        if (resultado.status == HttpStatusCode.Ok) {
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('usuarioActual')
+          this.usuariosService.UsuarioIngresando.emit(null);
+          this.router.navigate((['/']));
+        }
+      });
+  }
+
+  esUsuarioAdministrador = () => this.usuario?.tipo == TipoUsuario.Administrador;
+
+  esUsuarioCliente = () => this.usuario?.tipo == TipoUsuario.Cliente;
 }
