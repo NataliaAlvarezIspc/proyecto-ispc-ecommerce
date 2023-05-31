5462f6ae1a663c71f384b7bb388854970d6f8844
Roberto <rpgrca@gmail.com>
2023-05-31 17:09:25 -0300

    agregar componente de usuarios

--- front-end/enbalde/src/app/pages/usuarios/usuarios.component.ts
+++ front-end/enbalde/src/app/pages/usuarios/usuarios.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import { Usuario } from 'src/app/models/modelo.usuario';
+
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.css']
+  styleUrls: ['./usuarios.component.css'],
+  providers: [ UsuariosService ]
 })
-export class UsuariosComponent {
 
+export class UsuariosComponent {
+  @Input() usuarios: Usuario[] = [];
+
+  constructor(private formBuilder: FormBuilder, private usuariosService: UsuariosService) {
+  }
+
+  ngOnInit(): void {
+    this.usuariosService.obtenerUsuarios()
+      .subscribe((usuarios: Usuario[]) => this.usuarios = usuarios);
+  }
 }
