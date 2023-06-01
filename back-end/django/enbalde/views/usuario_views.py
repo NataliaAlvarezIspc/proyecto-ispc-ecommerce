a3b9adee08e68f9fedd4d6ee3e5ff643d354dab5
Roberto <rpgrca@gmail.com>
2023-05-31 22:57:16 -0300

    quitar import innecesario

--- back-end/django/enbalde/views/usuario_views.py
+++ back-end/django/enbalde/views/usuario_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import generics, status
 from rest_framework.serializers import ValidationError
-from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
