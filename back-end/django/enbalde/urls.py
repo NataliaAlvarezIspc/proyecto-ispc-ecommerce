78d944a462afdc9cec2555bdddcb69a86211dd61
Roberto <rpgrca@gmail.com>
2023-06-01 01:06:35 -0300

    custom put para manejar la imagen en articulo

--- back-end/django/enbalde/urls.py
+++ back-end/django/enbalde/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
+from django.conf import settings
+from django.core.files.storage import default_storage
 from rest_framework import routers, viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Usuario, Articulo, TipoArticulo
 from .serializers import UsuarioSerializer, ArticuloSerializer, TipoArticuloSerializer
 from .views.usuario_views import LoginView, LogoutView, SignupView
-from .views.tipo_articulo_views import UnTipoArticulo
-from .models import Usuario
+from .views.common import generar_nombre_unico
 
 
 class UsuarioViewSet(viewsets.ModelViewSet):
@@ -15,7 +19,30 @@ class UsuarioViewSet(viewsets.ModelViewSet):
 class ArticuloViewSet(viewsets.ModelViewSet):
     queryset = Articulo.objects.all()
     serializer_class = ArticuloSerializer
-    
+
+    def update(self, request: Request, *args, **kwargs):
+        try:
+            articulo_existente = self.get_object()
+            articulo_existente.nombre = request.data.get('nombre')
+            articulo_existente.descripcion = request.data.get('descripcion')
+            articulo_existente.precio = request.data.get('precio')
+            articulo_existente.cantidad = request.data.get('cantidad')
+            articulo_existente.costo = request.data.get('costo')
+            articulo_existente.tipo = TipoArticulo.objects.get(pk=request.data.get('tipo'))
+
+            contenido_imagen = request.FILES.get('imagen')
+            if contenido_imagen:
+                imagen = generar_nombre_unico(contenido_imagen.name)
+                camino = default_storage.save(f"{settings.MEDIA_ROOT}/images/{imagen}", contenido_imagen)
+                articulo_existente.imagen = camino
+
+            articulo_existente.save()
+            serializer = ArticuloSerializer(articulo_existente)
+            return Response(serializer.data, status.HTTP_201_CREATED)
+
+        except Exception as ex:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 # TODO: validar que no se creen dos iguales?
 class TipoArticuloViewSet(viewsets.ModelViewSet):
