bc53ea687f352fec3f157128fee2760ddc659a90
Roberto <rpgrca@gmail.com>
2023-06-06 22:24:44 -0300

    conectarse con el servidor para agregar cosas al carrito

--- front-end/enbalde/src/app/services/carrito.service.ts
+++ front-end/enbalde/src/app/services/carrito.service.ts
@@ -2,17 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Seleccion } from '../models/modelo.seleccion';
+import { Producto } from '../models/modelo.producto';
+import { AuthService } from './auth.service';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class CarritoService {
-  private carritoUrl: string = 'assets/carrito.json';
+  private API_URL = environment.API_URL;
+  private carritoUrl: string = `${this.API_URL}/carritos/`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   obtenerProductosCarrito(): Observable<Seleccion[]> {
     return this.http.get<Seleccion[]>(this.carritoUrl);
+  }
+
+  agregarProductoAlCarrito(producto: Producto): Observable<boolean> {
+    let carrito = this.authService.obtenerCarritoActual();
+    return this.http.put<boolean>(`${this.carritoUrl}${carrito}`, { articulo: producto.id, cantidad: 1 })
   }
 }
