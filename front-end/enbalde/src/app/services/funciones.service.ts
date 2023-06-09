8f9cce4e28ef91068f284b51f563808dc5561c19
Roberto <rpgrca@gmail.com>
2023-06-08 21:11:16 -0300

    agregar funcion para formatear fechas

--- front-end/enbalde/src/app/services/funciones.service.ts
+++ front-end/enbalde/src/app/services/funciones.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Inject, Injectable, LOCALE_ID } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionesService {
 
-  constructor() { }
+  constructor(@Inject(LOCALE_ID) private locale: string) { }
 
   crearId = (id: number) => `id-${id}`;
 
   extraerId = (id: string) => parseInt(id.split("-")[1]);
+
+  visualizarFecha = (fecha: Date) => formatDate(fecha, 'yyyy-MM-dd hh:mm', this.locale);
 }
