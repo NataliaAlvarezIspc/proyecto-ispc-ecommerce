50c7bfa99bda23341e3146380e03e9e4e533dfb5
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-06-09 00:15:48 -0300

    modificar constructor de url y cambiar ResultadoApi por Usuario

--- front-end/enbalde/src/app/services/usuarios.service.ts
+++ front-end/enbalde/src/app/services/usuarios.service.ts
@@ -12,7 +12,7 @@ import { environment } from 'src/environment/environment';
 export class UsuariosService {
   private API_URL = environment.API_URL;
   private registracionUrl: string = `${this.API_URL}/auth/signup/`;
-  private usuariosUrl: string = `${this.API_URL}/usuarios`;
+  private usuariosUrl: string = `${this.API_URL}/usuarios/`;
 
   constructor(private http: HttpClient) {
   }
@@ -55,7 +55,7 @@ export class UsuariosService {
     return true;
   }
 
-  modificar(usuario: Usuario, nuevaDireccion: string, nuevoEmail: string, nuevaClave: string, nuevoTelefono: string, nuevasObservaciones: string): Observable<ResultadoApi> {
+  modificar(usuario: Usuario, nuevaDireccion: string, nuevoEmail: string, nuevaClave: string, nuevoTelefono: string, nuevasObservaciones: string): Observable<Usuario> {
     const formData = new FormData();
     formData.append('direccion', nuevaDireccion);
     formData.append('email', nuevoEmail);
@@ -63,18 +63,10 @@ export class UsuariosService {
     formData.append('telefono', nuevoTelefono);
     formData.append('observaciones', nuevasObservaciones);
 
-    const url = `${this.usuariosUrl}/profile/${usuario.id}/`;
+    const url = `${this.usuariosUrl}${usuario.id}/`;
 
-    return this.http.patch<ResultadoApi>(url, formData)
-    .pipe(catchError(error => {
-      const resultado: ResultadoApi = {
-        mensaje: error.error.mensaje,
-        data: error.error.data,
-        status: error.error.status
-      };
-
-      return throwError(() => resultado);
-    }));
+    return this.http.patch<Usuario>(url, formData);
+    
   }
   
 
