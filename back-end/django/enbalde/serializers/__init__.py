e1734b192bf9d12fc8b84cf421ce6ff1014fbb48
Roberto <rpgrca@gmail.com>
2023-06-10 19:16:58 -0300

    exportar en __all__ para borrar F401 error

--- back-end/django/enbalde/serializers/__init__.py
+++ back-end/django/enbalde/serializers/__init__.py
@@ -6,3 +6,6 @@ from .carrito_serializer import CarritoSerializer
 from .seleccion_serializer import SeleccionSerializer
 from .venta_serializer import VentaSerializer
 from .registro_serializer import RegistroSerializer
+
+__all__ = ("TipoArticuloSerializer", "OfertaSerializer", "UsuarioSerializer", "ArticuloSerializer", "CarritoSerializer",
+           "SeleccionSerializer", "VentaSerializer", "RegistroSerializer")
