0c70528cd4c46be7c2379b46781eb2d10cd12db0
Roberto <rpgrca@gmail.com>
2023-06-12 17:49:06 -0300

    enviar informacion de descuento al cliente

--- back-end/django/enbalde/serializers/seleccion_serializer.py
+++ back-end/django/enbalde/serializers/seleccion_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from ..models import Seleccion
+from ..models import Seleccion, Oferta
 from .articulo_serializer import ArticuloSerializer
+from .oferta_serializer import OfertaSerializer
 
 
 class SeleccionSerializer(serializers.ModelSerializer):
@@ -9,3 +10,15 @@ class SeleccionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seleccion
         fields = ['id', 'cantidad', 'articulo']
+
+    def to_representation(self, instance: Seleccion):
+        representation = super().to_representation(instance)
+
+        ofertas = Oferta.objects.filter(articulos=instance.articulo)
+        ls = []
+        for oferta in ofertas:
+            serializer = OfertaSerializer(instance=oferta)
+            ls.append({ "nombre": serializer.data["nombre"], "descuento": serializer.data["descuento"] })
+
+        representation.update({ "ofertas": ls })
+        return representation
