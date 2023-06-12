46cadf40fe4a8a44416aaf03c769a73ef17685b7
Roberto <rpgrca@gmail.com>
2023-06-12 00:42:40 -0300

    crear serializador de ids de articulos

--- back-end/django/enbalde/serializers/oferta_serializer.py
+++ back-end/django/enbalde/serializers/oferta_serializer.py
@@ -1,10 +1,21 @@
+from collections import OrderedDict
 from rest_framework import serializers
-from ..models import Oferta
+from ..models import Oferta, Articulo
+from .articulo_serializer import ArticuloIdSerializer, ArticuloSerializer
 
 
 class OfertaSerializer(serializers.ModelSerializer):
     fechaVencimiento = serializers.DateTimeField(required=True, source="fecha_vencimiento")
+    articulos = ArticuloIdSerializer(read_only=False, many=True)
 
     class Meta:
         model = Oferta
-        fields = ['id', 'nombre', 'descuento', 'fechaVencimiento']
+        fields = ['id', 'nombre', 'descuento', 'fechaVencimiento', 'articulos']
+
+    def create(self, validated_data: OrderedDict):
+        articulos = [Articulo.objects.get(pk=articulo["id"]) for articulo in validated_data["articulos"]]
+        oferta = Oferta.objects.create(nombre=validated_data["nombre"], descuento=validated_data["descuento"],
+                                       fecha_vencimiento=validated_data["fecha_vencimiento"])
+        oferta.articulos.set(articulos)
+        oferta.save()
+        return oferta
