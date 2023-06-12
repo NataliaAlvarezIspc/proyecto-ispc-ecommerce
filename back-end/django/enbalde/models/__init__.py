ed8afe14469ebbc5f33c2a05a4d62c3fdb7e037e
Roberto <rpgrca@gmail.com>
2023-06-12 01:11:31 -0300

    eliminar ArticulosEnOferta

--- back-end/django/enbalde/models/__init__.py
+++ back-end/django/enbalde/models/__init__.py
@@ -4,10 +4,9 @@ from .envio import Envio
 from .oferta import Oferta
 from .usuario import Usuario
 from .articulo import Articulo
-from .articulo_oferta import ArticulosEnOferta
 from .carrito import Carrito
 from .seleccion import Seleccion
 from .venta import Venta
 
 __all__ = ("aceptar_solo_fechas_futuras", "aceptar_solo_fechas_pasadas", "TipoArticulo", "Envio", "Oferta", "Usuario",
-           "Articulo", "ArticulosEnOferta", "Carrito", "Seleccion", "Venta")
+           "Articulo", "Carrito", "Seleccion", "Venta")
