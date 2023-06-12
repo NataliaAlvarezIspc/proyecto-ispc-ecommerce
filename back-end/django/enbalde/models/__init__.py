49eb392900363bb7d211139f7a68ec91dda6a47c
Roberto <rpgrca@gmail.com>
2023-06-12 13:46:07 -0300

    agregar metodo para resetear password

--- back-end/django/enbalde/models/__init__.py
+++ back-end/django/enbalde/models/__init__.py
@@ -1,4 +1,5 @@
 from .common import aceptar_solo_fechas_futuras, aceptar_solo_fechas_pasadas
+from .password_reset import password_reset_token_created
 from .tipo_articulo import TipoArticulo
 from .envio import Envio
 from .oferta import Oferta
@@ -8,5 +9,5 @@ from .carrito import Carrito
 from .seleccion import Seleccion
 from .venta import Venta
 
-__all__ = ("aceptar_solo_fechas_futuras", "aceptar_solo_fechas_pasadas", "TipoArticulo", "Envio", "Oferta", "Usuario",
-           "Articulo", "Carrito", "Seleccion", "Venta")
+__all__ = ("aceptar_solo_fechas_futuras", "aceptar_solo_fechas_pasadas", "password_reset_token_created", "TipoArticulo",
+           "Envio", "Oferta", "Usuario", "Articulo", "Carrito", "Seleccion", "Venta")
