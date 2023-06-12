856422e8b9ef4773f58fe0b8af5193ce99c8563e
Roberto <rpgrca@gmail.com>
2023-06-12 01:08:31 -0300

    sobreescribir update para serializar oferta en put

--- back-end/django/enbalde/serializers/oferta_serializer.py
+++ back-end/django/enbalde/serializers/oferta_serializer.py
@@ -13,9 +13,21 @@ class OfertaSerializer(serializers.ModelSerializer):
         fields = ['id', 'nombre', 'descuento', 'fechaVencimiento', 'articulos']
 
     def create(self, validated_data: OrderedDict):
-        articulos = [Articulo.objects.get(pk=articulo["id"]) for articulo in validated_data["articulos"]]
+        articulos = self._obtener_articulos_de_datos(validated_data)
         oferta = Oferta.objects.create(nombre=validated_data["nombre"], descuento=validated_data["descuento"],
                                        fecha_vencimiento=validated_data["fecha_vencimiento"])
         oferta.articulos.set(articulos)
         oferta.save()
         return oferta
+
+    def _obtener_articulos_de_datos(self, validated_data):
+        return [Articulo.objects.get(pk=articulo["id"]) for articulo in validated_data["articulos"]]
+
+    def update(self, instance: Oferta, validated_data):
+        articulos = self._obtener_articulos_de_datos(validated_data)
+        instance.nombre = validated_data["nombre"]
+        instance.descuento = validated_data["descuento"]
+        instance.fecha_vencimiento = validated_data["fecha_vencimiento"]
+        instance.articulos.set(articulos)
+        instance.save()
+        return instance
