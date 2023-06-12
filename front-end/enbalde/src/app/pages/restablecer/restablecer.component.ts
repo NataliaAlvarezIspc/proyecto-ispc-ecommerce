0712c06292a6959702e6d666fac3cec83ef03e1a
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-06-12 14:43:39 -0300

    arreglar regex de mail en restablecer

--- front-end/enbalde/src/app/pages/restablecer/restablecer.component.ts
+++ front-end/enbalde/src/app/pages/restablecer/restablecer.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 
 export class RestablecerComponent implements OnInit {
   restablecerForm = new FormGroup({
-    mail: new FormControl ("", [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')]),
+    mail: new FormControl ("", [Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
   })
 
   ngOnInit(): void {
