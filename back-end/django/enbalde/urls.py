d54145b411dd7075f4e9383c5d6d8c01b50ca554
Roberto <rpgrca@gmail.com>
2023-06-12 13:46:36 -0300

    agregar reset de password en urls

--- back-end/django/enbalde/urls.py
+++ back-end/django/enbalde/urls.py
@@ -146,6 +146,7 @@ urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='auth_login'),
     path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
     path('auth/signup/', SignupView.as_view(), name='auth_signup'),
+    path('auth/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     path('carritos/<int:pk>', UnCarrito.as_view()),
     path('carritos/', Carritos.as_view()),
     path('compras/<int:pk>', Compras.as_view()),
