bb131a4745697d7e162f1911aaee95b87f67970e
Roberto <rpgrca@gmail.com>
2023-06-12 14:32:11 -0300

    agregar formulario para cambiar clave por token

--- front-end/enbalde/src/app/pages/restablecer/restablecer.component.ts
+++ front-end/enbalde/src/app/pages/restablecer/restablecer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 import { Router } from '@angular/router';
@@ -11,8 +11,15 @@ import { Router } from '@angular/router';
 })
 
 export class RestablecerComponent implements OnInit {
+  @Input() errores = [];
+
   restablecerForm = new FormGroup({
     mail: new FormControl ("", [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')]),
+  })
+
+  resetForm = new FormGroup({
+    token: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required])
   })
 
   ngOnInit(): void {
@@ -20,14 +27,35 @@ export class RestablecerComponent implements OnInit {
 
   get mail() { return this.restablecerForm.get('mail'); }
 
-  constructor (private usuariosService: UsuariosService, private router: Router, private elementRef: ElementRef) {}
+  get token() { return this.resetForm.get('token'); }
+
+  get password() { return this.resetForm.get('password'); }
+
+  constructor (private usuariosService: UsuariosService, private router: Router, private elementRef: ElementRef) {
+  }
 
   onSubmit(value: any): void {
     this.usuariosService.restablecerClave(value.mail)
       .subscribe(_ => {
-        alert('Si su mail se encuentra en nuestra base de datos, le será enviada una nueva contraseña.');
-        this.router.navigate(['/']);
-        this.elementRef.nativeElement.ownerDocument.documentElement.scrollTop = 0;
+        alert('Si su mail se encuentra en nuestra base de datos recibirá un token para cambiar su clave.');
+      })
+  }
+
+  onReset(value: any) {
+    this.usuariosService.cambiarClavePorReset(value.token, value.password)
+      .subscribe({
+        next: exito => {
+          console.log(exito);
+          alert('Su clave ha sido cambiada exitosamente. Puede ingresar.');
+          this.router.navigate(['/login/']);
+          this.elementRef.nativeElement.ownerDocument.documentElement.scrollTop = 0;
+        },
+        error: error => {
+          this.errores = error["password"];
+          setTimeout(() => {
+            this.errores = [];
+          }, 3000);
+        }
       })
   }
 }
