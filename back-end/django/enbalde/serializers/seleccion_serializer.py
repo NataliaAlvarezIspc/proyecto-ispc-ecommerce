57dd5a68d5138e9ad7195fddc06e70fca871d978
Roberto <rpgrca@gmail.com>
2023-06-12 17:53:08 -0300

    agregar informacion de total y descuento en seleccion

--- back-end/django/enbalde/serializers/seleccion_serializer.py
+++ back-end/django/enbalde/serializers/seleccion_serializer.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 from ..models import Seleccion, Oferta
 from .articulo_serializer import ArticuloSerializer
@@ -13,12 +14,16 @@ class SeleccionSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance: Seleccion):
         representation = super().to_representation(instance)
-
         ofertas = Oferta.objects.filter(articulos=instance.articulo)
+        descuento = 0
         ls = []
+
         for oferta in ofertas:
             serializer = OfertaSerializer(instance=oferta)
             ls.append({ "nombre": serializer.data["nombre"], "descuento": serializer.data["descuento"] })
+            descuento += (instance.articulo.precio * (Decimal(serializer.data["descuento"]) / Decimal(100))) * instance.cantidad
 
         representation.update({ "ofertas": ls })
+        representation.update({ "descuento": descuento })
+        representation.update({ "total": (instance.articulo.precio * instance.cantidad) - descuento })
         return representation
