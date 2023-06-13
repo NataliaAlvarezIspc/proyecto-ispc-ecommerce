020e22e6b87d9055e08d96889c2e173870c90fe8
Roberto <rpgrca@gmail.com>
2023-06-13 17:31:55 -0300

    agregar transaccion y pago al modelo del front

--- front-end/enbalde/src/app/models/modelo.venta.ts
+++ front-end/enbalde/src/app/models/modelo.venta.ts
@@ -1,9 +1,17 @@
 import { Seleccion } from "./modelo.seleccion";
 
+export enum TipoPago {
+  EFECTIVO_A_PAGAR = 1,
+  EFECTIVO_PAGADO = 2,
+  ENBALDE_PAGO = 3
+}
+
 export interface Venta {
   cliente: string;
   fecha: Date,
   selecciones: Seleccion[];
   total: number;
   envio: string;
+  pago: TipoPago;
+  transaccion: string;
 }
