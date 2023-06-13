d3230b9042d6fa2780cec1976879d88cb694e072
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-13 01:39:59 -0300

    [ADD] Back-end actualizado

--- back-end/django/server/settings.py
+++ back-end/django/server/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'troncosobraianaxel@gmail.com'
-EMAIL_HOST_PASSWORD = 'cwogzeyvyacgevbd'
+EMAIL_HOST_USER = 'TUEMAIL'
+EMAIL_HOST_PASSWORD = 'ACACOLOCARHASH'
