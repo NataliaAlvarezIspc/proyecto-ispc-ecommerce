0263ecbb34795646fc4b3d9d31bda6d3e1a1aab1
Roberto <rpgrca@gmail.com>
2023-06-13 11:02:31 -0300

    borrar clave

--- back-end/django/server/settings.py
+++ back-end/django/server/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enbalde',
         'USER': 'root',
-        'PASSWORD': 'braianmysql',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
