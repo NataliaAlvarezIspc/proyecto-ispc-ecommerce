69e78cf74381551393d2c0293c567fc8f314ebcc
Roberto <rpgrca@gmail.com>
2023-06-13 00:03:41 -0300

    usar template de resultado-api para tipo de producto

--- front-end/enbalde/src/app/pages/tipo-producto/tipo-producto.component.ts
+++ front-end/enbalde/src/app/pages/tipo-producto/tipo-producto.component.ts
@@ -14,16 +14,12 @@ import { HttpStatusCode } from '@angular/common/http';
 
 export class TipoProductoComponent {
   crearTipoProductoForm!: FormGroup;
-  mostrarMensajeExitoso : boolean = false;
-  @Input() tipoProductos: TipoProducto[] = [];
-  @Input() resultado: ResultadoApi;
+  @Input() tipoProductos: TipoProducto[];
+  @Input() resultado?: ResultadoApi;
 
   constructor(private formBuilder: FormBuilder, private productosService: ProductosService) {
-    this.resultado = {
-      mensaje: "",
-      data: {},
-      status: 0 as HttpStatusCode
-    }
+    this.tipoProductos = [];
+    this.resultado = undefined;
   }
 
   ngOnInit(): void {
@@ -35,22 +31,16 @@ export class TipoProductoComponent {
 
   crear(value: any) {
     this.productosService.crearTipo(value.nombre)
-      .subscribe({
-        next: (exito: ResultadoApi) => { this.resultado = exito; this.refrescar(); 
-        this.mostrarMensajeExitoso = true;
-        setTimeout(()=>{
-          this.mostrarMensajeExitoso = false;
-        }, 3000);
+      .subscribe((tipoProducto: TipoProducto) => {
+        this.refrescar({ mensaje: "Tipo creado con éxito.", data: {}, status: HttpStatusCode.Ok });
         this.crearTipoProductoForm.reset();
-        },
-        error: (error: ResultadoApi) => { this.resultado = error; },
-        complete: () => {}
       });
   }
 
   get nombre() { return this.crearTipoProductoForm.get('nombre'); }
 
-  refrescar() {
+  refrescar(resultado?: ResultadoApi) {
+    this.resultado = resultado;
     this.productosService.obtenerTipos()
       .subscribe((tipoProductos: TipoProducto[]) => this.tipoProductos = tipoProductos);
   }
