eef57711ea76452b288a495df2f4439deac79967
Roberto <rpgrca@gmail.com>
2023-06-13 11:06:16 -0300

    quitar password

--- back-end/django/server/settings.py
+++ back-end/django/server/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'enbalde',
         'USER': 'root',
-        'PASSWORD': 'braianmysql',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
