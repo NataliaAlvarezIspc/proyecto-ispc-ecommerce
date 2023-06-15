ac77cb5ce14935252c6155730820c0c99e273c65
Roberto <rpgrca@gmail.com>
2023-06-14 21:18:11 -0300

    agregar servicio de enbaldepago

--- front-end/enbalde/src/app/services/enbalde-pago.service.ts
+++ front-end/enbalde/src/app/services/enbalde-pago.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Envio } from '../models/modelo.envio';
 import { environment } from 'src/environment/environment';
 import { HttpClient } from '@angular/common/http';
-import { AuthService } from './auth.service';
 import { AutorizacionEnbaldePago } from '../models/modelo.autorizacionenbaldepago';
 import { Seleccion } from '../models/modelo.seleccion';
-import { TipoPago } from '../models/modelo.venta';
-import { AutorizacionPago } from '../models/modelo.autorizacionpago';
 
 @Injectable({
   providedIn: 'root'
@@ -15,21 +12,21 @@ import { AutorizacionPago } from '../models/modelo.autorizacionpago';
 
 export class EnbaldePagoService {
   private API_URL = environment.ENBALDEPAGO_API_URL;
+  private autorizacionUrl: string = `${this.API_URL}/autorizacion/`;
   private pagoUrl: string = `${this.API_URL}/pago/`;
 
-  constructor(private http: HttpClient, private authService: AuthService) {
+  constructor(private http: HttpClient) {
   }
 
-  autorizar(carrito: Seleccion[], envio: Envio): Observable<AutorizacionPago> {
+  autorizar(carrito: Seleccion[], envio: Envio): Observable<any> {
     let total = carrito.reduce((t, s) => t + s.total, 0) + envio.monto;
     let descripcion = carrito.map(s => `${s.articulo.nombre} x ($ ${s.articulo.precio} - $ ${s.descuento}) x ${s.cantidad} = $ ${s.total}`).join('\n');
     descripcion += `\nEnvío: ${envio.nombre} ($ ${envio.monto})`;
 
-    return this.http.post<AutorizacionEnbaldePago>(this.pagoUrl, { descripcion, total })
-      .pipe(
-        map((res: AutorizacionEnbaldePago) =>
-          ({ tipo: TipoPago.ENBALDE_PAGO, transaccion: res.transaccion, status: res.status, mensaje: res.mensaje }))
-      );
+    return this.http.post(this.autorizacionUrl, { descripcion, total });
+  }
+
+  pagar(respuesta: string, ticket: string) {
+    return this.http.post<AutorizacionEnbaldePago>(this.pagoUrl, { respuesta, ticket });
   }
 }
-
