27aa19a0c838859b64d7671ec97d2baf7f770cf5
Roberto <rpgrca@gmail.com>
2023-06-15 22:10:02 -0300

    darle permiso al formulario de contacto, usar mail local

--- back-end/django/enbalde/views/contacto_view.py
+++ back-end/django/enbalde/views/contacto_view.py
@@ -1,20 +1,23 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from django.core.mail import send_mail
+from server.settings import EMAIL_HOST_USER
 
 
 class ContactoView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         reason = request.POST.get('reason')
         message = request.POST.get('message')
 
-        asunto = f"Nuevo mensaje de contacto - {reason}"
+        asunto = f"Nuevo mensaje de Enbalde Contacto - {reason}"
         contenido = f"Nombre: {name}\nCorreo: {email}\nMensaje: {message}"
-        remitente = 'admin@enbalde.local'
-        destinatario = ['admin@enbalde.local']
+        remitente =  EMAIL_HOST_USER
+        destinatario = [EMAIL_HOST_USER]
 
         send_mail(asunto, contenido, remitente, destinatario)
         return Response(status=status.HTTP_200_OK)
