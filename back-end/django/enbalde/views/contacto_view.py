566e4cdbc6a35a3c692b3da9b1bbd7e2a0109ed3
Roberto <rpgrca@gmail.com>
2023-06-15 22:11:56 -0300

    atrapar posible error y retornar 400

--- back-end/django/enbalde/views/contacto_view.py
+++ back-end/django/enbalde/views/contacto_view.py
@@ -8,16 +8,20 @@ from server.settings import EMAIL_HOST_USER
 
 class ContactoView(APIView):
     permission_classes = [AllowAny]
+
     def post(self, request):
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        reason = request.POST.get('reason')
-        message = request.POST.get('message')
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            reason = request.POST.get('reason')
+            message = request.POST.get('message')
 
-        asunto = f"Nuevo mensaje de Enbalde Contacto - {reason}"
-        contenido = f"Nombre: {name}\nCorreo: {email}\nMensaje: {message}"
-        remitente =  EMAIL_HOST_USER
-        destinatario = [EMAIL_HOST_USER]
+            asunto = f"Nuevo mensaje de Enbalde Contacto - {reason}"
+            contenido = f"Nombre: {name}\nCorreo: {email}\nMensaje: {message}"
+            remitente =  EMAIL_HOST_USER
+            destinatario = [EMAIL_HOST_USER]
 
-        send_mail(asunto, contenido, remitente, destinatario)
-        return Response(status=status.HTTP_200_OK)
+            send_mail(asunto, contenido, remitente, destinatario)
+            return Response(status=status.HTTP_200_OK)
+        except Exception as ex:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
